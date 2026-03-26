4ef2f6f1ae19594b4d13dff470e4ba98013638e6
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-03-27 01:11:59 +0600

    sticky behaviour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router";
 import user from "../assets/user.png";
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center ">
       <div className=""></div>
       <div className="nav flex gap-5 text-accent">
         <NavLink to="/">Home</NavLink>
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -14,18 +14,18 @@ const HomeLayout = () => {
         <section className="w-11/12 mx-auto my-3">
           <LatestNews></LatestNews>
         </section>
-        <nav className="w-11/12 mx-auto my-3">
+        <nav className="w-11/12 mx-auto my-3 ">
           <Navbar></Navbar>
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
         <section className="main col-span-6">
           <Outlet></Outlet>
         </section>
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit ">
           <RighAside></RighAside>
         </aside>
       </main>
